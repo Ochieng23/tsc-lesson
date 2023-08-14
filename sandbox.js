ee9e0705409dc9f1cc288e5411efac23ddd97437
paul <ochiengpaul001@gmail.com>
2023-08-14 16:10:33 +0300

    data types and explicit opertations

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,35 @@
 var age = 30;
 console.log(age);
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(5));
+var ChemMarks = 40;
+var PhycMarks = 50;
+var total = ChemMarks + PhycMarks;
+console.log(total);
+var isLovley = true;
+isLovley = false;
+console.log(isLovley);
+//Arrays 
+var languagesk = ['javascript', 'typescript', 'ruby'];
+languagesk.push('python');
+console.log(languagesk);
+//objects 
+var detEmployee = {
+    name: 'paul',
+    age: 26,
+    street: 'Entebbe',
+    maiden: 'jane'
+};
+detEmployee.name = 'Omondi';
+console.log(detEmployee);
+//explicits in functions 
+var addItion = function (a, b) {
+    return a + b;
+};
+console.log(addItion(3, 4));
+var adds = function (a, b) {
+    return a + b;
+};
+console.log(adds(3, 2));
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,56 @@
+
+
 let age: number = 30; 
 
 console.log(age)
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(5)); 
+
+const ChemMarks: number = 40;
+const PhycMarks: number = 50;
+
+const total = ChemMarks + PhycMarks
+console.log(total)
+
+let isLovley = true;
+
+isLovley = false;
+
+console.log(isLovley)
+
+//Arrays 
+ let languagesk = ['javascript', 'typescript', 'ruby'];
+
+languagesk.push('python')
+
+console.log(languagesk)
+
+//objects 
+let detEmployee = {
+    name: 'paul',
+    age: 26,
+    street: 'Entebbe',
+    maiden: 'jane'
+}
+detEmployee.name = 'Omondi'
+console.log(detEmployee)
+
+//explicits in functions 
+
+const addItion = (a:number,b:number)=>{
+    return a + b 
+}
+console.log(addItion(3,4))
+
+// explicit type 
+
+type mathCalculation = (a:number, b:number) => number; 
+
+const adds: mathCalculation = (a,b)=> {
+    return a+b}; 
+
+console.log(adds(3,2))
